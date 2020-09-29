45e998c83733ab04215e79f40b3e8d10abaaae35
Georgios Fotakis <fotakisg@gmail.com>
2020-09-29 10:48:00 +0200

    Update count_fastq_reads.sh

--- count_fastq_reads.sh
+++ count_fastq_reads.sh
@@ -5,6 +5,8 @@
 # Counts how many reads are in a FASTQ file. 
 ##########################################################
 
+FASTQ_FILE=$1
+
 if [[ $FASTQ_FILE =~ \.gz$ ]]; then
     zcat "$FASTQ_FILE" | grep  "^@" | wc -l
 else
